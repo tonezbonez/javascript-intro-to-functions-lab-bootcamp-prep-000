09439df4a070c3115b540ff0034175b9f23afc3e
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-14 17:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,10 @@ function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 logShout('Hello')
+
+function sayHiToGrandma(string){
+  if string='hello'{console.log('I can\'t hear you!')}
+  else if string = 'HELLO'{console.log('YES INDEED!')}
+  else if string = 'I love you, Grandma.'{console.log('I love you, too.')}
+  else {console.log('invalid input')}
+}
