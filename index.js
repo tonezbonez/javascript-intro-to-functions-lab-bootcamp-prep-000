107b012872d8f638cc90284c24715eb7b4f7c4dc
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-14 16:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-+function shout(string){
- +  return string.toUpperCase()
- +};
- +
- +shout("hello");
- +
- +function logShout(string){
- +  return console.log(string.toUpperCase())
- +}
- +
- +logShout("hello")
- +
- +function whisper(string){
- +  return string.toLowerCase();
- +}
- +
- +whisper("HELLO")
- +
- +function logWhisper(string){
- +  return console.log(string.toLowerCase())
- +}
- +
- +logWhisper("HELLO")
+function shout(string){
+  return string.toUpperCase()
+ };
+
+ shout("hello");
+
+ function logShout(string){
+   return console.log(string.toUpperCase())
+}
+
+ logShout("hello")
+
+ function whisper(string){
+   return string.toLowerCase();
+ }
+
+ whisper("HELLO")
+
+ function logWhisper(string){
+   return console.log(string.toLowerCase())
+}
+
+ logWhisper("HELLO")
